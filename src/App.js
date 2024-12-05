8ba9570dda022822997a6252efbbc9046820af2c
Thuyvan31803 <lethuyvan3182003@gmail.com>
2024-12-05 09:13:23 +0700

    Add new project TaskManagementApplication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+let nextId = 4;
+const initialArtists = [
+  { id: 0, name: 'Complete React Tutorial' },
+  { id: 1, name: 'Build Portfolio'},
+  { id: 2, name: 'Learn Lodash'},
+  { id: 3, name: 'Write Documentation'},
+];
+
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState(
+    initialArtists
+  );
+
+  function handleClick() {
+    const insertAt = 1; // Could be any index
+    const nextArtists = [
+      // Items before the insertion point:
+      ...artists.slice(0, insertAt),
+      // New item:
+      { id: nextId++, name: name },
+      // Items after the insertion point:
+      ...artists.slice(insertAt)
+    ];
+    setArtists(nextArtists);
+    setName('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Task Manager</h1>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={handleClick}>
+        Insert
+      </button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}
+          <ItemList
+        artworks={name}
+        onToggle={handleClick} />
+          <button onClick={() => {
+            setArtists(
+              artists.filter(a =>
+                a.id !== artist.id
+              )
+            );
+          }}>Delete
+          </button>
+          </li>
+           
+        ))}
+      </ul>
+    </>
   );
 }
-
-export default App;
